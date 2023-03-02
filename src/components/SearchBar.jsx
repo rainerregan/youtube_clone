8944d365cdc42debfdc09a7225c489ea3cf23676
Rainer Regan <rainercoc@gmail.com>
2023-03-02 20:15:57 +0700

    Fixing Unused Vars

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Paper, IconButton, Icon } from '@mui/material'
+import { Paper, IconButton } from '@mui/material'
 import Search from '@mui/icons-material/Search'
 
 const SearchBar = () => {
